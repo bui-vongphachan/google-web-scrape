c0c581c884b5baaa0d884fc1466b5da1f716a4b0
vongphachan~ <vongphachannukkalash@gmail.com>
2024-03-27 19:17:31 +0700

    Refactor ExampleFileButton component to use anchor tag for file download

--- src/components/ExampleFileButton.tsx
+++ src/components/ExampleFileButton.tsx
@@ -1,27 +1,13 @@
-"use client";
-
 export default function ExampleFileButton() {
-  const onClick = async () => {
-    const response = await fetch("/api/fetchGoogle");
-
-    const data = await response.json();
-
-    console.log("response", response);
-
-    console.log("data", data);
-    /*     const corsProxyUrl = "https://cors-anywhere.herokuapp.com/";
-    const url = `${corsProxyUrl}https://www.google.com/search?q=openai+GPT-3`;
-
-    const response = await fetch(url, {
-      headers: { "x-requested-with": "xmlhttprequest" },
-    });
-    const html = await response.text();
-
-    console.log("html", html); */
-  };
   return (
-    <div>
-      <button onClick={onClick}> Download example file </button>
+    <div className=" flex items-center px-6">
+      <a
+        href="/sample-keyword.csv"
+        download="sample-keyword.csv"
+        className=" text-grey-300 hover:underline"
+      >
+        Click to Download
+      </a>
     </div>
   );
 }
